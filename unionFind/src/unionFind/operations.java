c26ddf26ff4c4529603f3016ff4d611a654f5960
haukk <haukk@DESKTOP-OASL68K>
2022-04-26 02:46:59 +0300

    Created class operations.

---
+++ unionFind/src/unionFind/operations.java
@@ -0,0 +1,17 @@
+package unionFind;
+
+public class operations {
+	
+	
+	static String[] array = {"kiuru", "lokki", "rastas", "sorsa", "varis"};
+	
+	
+	
+	//TODO: whole ckae lmaoo
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
